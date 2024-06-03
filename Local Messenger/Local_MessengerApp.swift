da647ca03ba9a074369d96cf4d731e927c56ee4d
Alex <aalex776@protonmail.com>
2024-06-03 11:56:50 -0400

    Initial Commit

---
+++ Local Messenger/Local_MessengerApp.swift
@@ -0,0 +1,18 @@
+//
+// Local_MessengerApp.swift
+// 
+// Created by Alwin Amoros on 6/3/24.
+// 
+
+	
+
+import SwiftUI
+
+@main
+struct Local_MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
